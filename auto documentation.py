011b798ccd00e29cd2863db94d01c57e844eec69
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-02-04 11:40:55 -0500

    Create auto documentation.py

---
+++ auto documentation.py
@@ -0,0 +1,7 @@
+# conf.py (Sphinx Config for Streamer Docs)
+project = "Streamer"
+author = "Violet Aura Creations"
+release = "1.0"
+
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+html_theme = "sphinx_rtd_theme"
