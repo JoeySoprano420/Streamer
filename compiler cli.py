74a2096a924ff50c25aa425d3e5fb2105187e808
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-02-04 11:14:43 -0500

    Create compiler cli.py

---
+++ compiler cli.py
@@ -0,0 +1,12 @@
+import sys
+
+def compile_streamer(input_file, output_file):
+    with open(input_file, 'r') as f:
+        code = f.read()
+    
+    binary_code = python_to_assembly(code)  
+    with open(output_file, 'wb') as f:
+        f.write(binary_code.encode())
+
+if __name__ == "__main__":
+    compile_streamer(sys.argv[1], sys.argv[2])
