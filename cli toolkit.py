49a3821342c3fce4619d7733df53d4e4495e919d
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-02-04 11:09:11 -0500

    Create cli toolkit.py

---
+++ cli toolkit.py
@@ -0,0 +1,13 @@
+import sys
+
+def compile_streamer(input_file, output_file):
+    with open(input_file, 'r') as f:
+        code = f.read()
+    
+    # Compile code (AST -> Assembly -> Binary)
+    binary_code = python_to_assembly(code)  
+    with open(output_file, 'wb') as f:
+        f.write(binary_code.encode())
+
+if __name__ == "__main__":
+    compile_streamer(sys.argv[1], sys.argv[2])
